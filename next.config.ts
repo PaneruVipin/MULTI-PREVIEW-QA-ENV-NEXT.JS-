8dacba0a086f69889109f37e633292336277125e
vp-payomatix <vipin@payomatix.com>
2025-11-15 20:03:10 +0530

    miner

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
-const basePath = process.env.NEXT_BASE_PATH || "";
 const nextConfig: NextConfig = {
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined,
-  /* config options here */
 };
 
 export default nextConfig;
